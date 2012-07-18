9a6988179a2499bb2b13c8a9b9f30e1448e0ad6a
Simon Vocella <voxsim@gmail.com>
2012-07-18 20:45:35 +0200

    add CreateImage class and build.xml

---
+++ src/CreateImage.java
@@ -0,0 +1,116 @@
+import java.awt.Graphics2D;
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+import java.awt.image.ColorModel;
+import java.awt.image.PixelGrabber;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import edu.emory.mathcs.jtransforms.dct.DoubleDCT_2D;
+
+public class CreateImage {
+	private BufferedImage firstimage;
+	private BufferedImage secondImage;
+	private BufferedImage thirdImage;
+
+	public CreateImage(String src, double offset, PrintWriter debug, boolean calc) {
+		try {
+			firstimage = ImageIO.read(new File(src));
+		
+			ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);  
+			ColorConvertOp op = new ColorConvertOp(cs, null);  
+			firstimage = op.filter(firstimage, null);
+			
+			int divide = 20;
+			
+			if(calc) {
+				double[][] pixels = CreateImage.getPixels(firstimage);
+		
+				long startS = new Date().getTime();
+				double[][] result = Dct.dct2(pixels, offset);
+				long endS =  new Date().getTime();
+				
+				debug.println("time on my dct2: "+(endS-startS));
+				
+				int n = pixels.length;
+				int m = pixels[0].length;
+				long startO = new Date().getTime();
+				DoubleDCT_2D dct_2d = new DoubleDCT_2D(n, m);
+				dct_2d.forward(pixels, true);
+				long endO = new Date().getTime();
+				
+				debug.println("time on jtransform dct2: "+(endO-startO));
+				
+				double[][] newPixels = Dct.idct2(result, -1*offset);
+				secondImage = CreateImage.setPixels(CreateImage.deepCopy(firstimage), newPixels);
+			} else {
+				secondImage = CreateImage.deepCopy(firstimage);
+			}
+			
+			thirdImage = new BufferedImage((firstimage.getWidth()*2)+divide, firstimage.getHeight(), BufferedImage.TYPE_INT_RGB);
+			
+			Graphics2D g2d = thirdImage.createGraphics();
+			
+			g2d.drawImage(firstimage, 0, 0, null);
+			g2d.drawImage(secondImage, firstimage.getWidth()+divide, 0, null);
+			
+			g2d.dispose();
+			
+			File file = new File(src+"-result.jpg");
+			ImageIO.write(thirdImage, "jpg", file);
+			
+			debug.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public static final boolean isGreyscaleImage(PixelGrabber pg) {
+        return pg.getPixels() instanceof byte[];
+    }
+	
+	public static double[][] getPixels(BufferedImage image) throws Exception {
+		int w = image.getWidth();
+		int h = image.getHeight();
+		WritableRaster raster = image.getRaster();
+		double[][] pixels = new double[w][h];
+		int[] buffer = new int[1];
+		
+		for( int i = 0; i < w; i++ ) {
+			for( int j = 0; j < h; j++ ) {
+				raster.getPixel(i, j, buffer);
+				pixels[i][j] = (double) buffer[0];
+			}
+		}
+		
+		return pixels;
+	}
+	
+	public static BufferedImage setPixels(BufferedImage image, double[][] pixels) throws Exception {
+		int w = image.getWidth();
+		int h = image.getHeight();
+		WritableRaster raster = image.getRaster();
+		int[] buffer = new int[1];
+		
+		for( int i = 0; i < w; i++ ) {
+			for( int j = 0; j < h; j++ ) {
+				buffer[0] = (int) pixels[i][j];
+				raster.setPixel(i, j, buffer);
+			}
+		}
+		
+		return image;
+	}
+	
+	static BufferedImage deepCopy(BufferedImage bi) {
+		 ColorModel cm = bi.getColorModel();
+		 boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+		 WritableRaster raster = bi.copyData(null);
+		 return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,8 @@
-import java.awt.color.ColorSpace;
-import java.awt.image.BufferedImage;
-import java.awt.image.ColorConvertOp;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.Date;
-
-import javax.imageio.ImageIO;
-//import javax.swing.JFrame;
-
-import edu.emory.mathcs.jtransforms.dct.DoubleDCT_2D;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		//JFrame frame = new JFrame("Display image");
-		//PanelImage image = new PanelImage();
-		//frame.getContentPane().add(image);
-		//frame.setVisible(true);
-		
 		PrintWriter out = new PrintWriter(new FileWriter("/home/simon/projects/discrete-cosine-transform/outputfile.txt")); 
 		
 		String src = "/home/simon/projects/discrete-cosine-transform/imgs/";
@@ -38,62 +23,11 @@ public class Main {
 				"spider_web.bmp",
 				"zone_plate.bmp"
 		};
+		double offset = -128.;
+		
 		for(int i=0; i<names.length; i++) {
-			//int x = 500;
-			//int y = 400;
-			//frame.setSize((x+10)*3, y);
-			//image.setXImage(x);
-			//image.setYImage(y);
-			//image.loadImage(src+names[i]);
-			
 			String input = src+names[i];
-			
-			BufferedImage firstimage = ImageIO.read(new File(input)); 
-			
-			ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);  
-			ColorConvertOp op = new ColorConvertOp(cs, null);  
-			BufferedImage secondImage = op.filter(firstimage, null);
-			
-			//double[][] pixels = image.getPixels();
-			
-			double[][] pixels = PanelImage.getPixels(secondImage);
-			
-			out.println("get pixels, now calculate dct2 on "+names[i]);
-			out.flush();
-			
-			out.println("ciao");
-			out.flush();
-			
-			double offset = -128.;
-			
-			long startS = new Date().getTime();
-			double[][] result = Dct.dct2(pixels, offset);
-			long endS =  new Date().getTime();
-			
-			out.println("time on my dct2: "+(endS-startS));
-			out.flush();
-			
-			//System.out.println("now dct2, now calculate idct2");
-			
-			//double[][] ipixels = Dct.idct2(result, -offset);
-			
-			//System.out.println("set pixels");
-			
-			//image.setPixels(ipixels);
-			
-			//System.out.println("tadaaam!");
-			
-			//image.repaint();
-			
-			int n = pixels.length;
-			int m = pixels[0].length;
-			long startO = new Date().getTime();
-			DoubleDCT_2D dct_2d = new DoubleDCT_2D(n, m);
-			dct_2d.forward(pixels, true);
-			long endO = new Date().getTime();
-			
-			out.println("time on jtransform dct2: "+(endO-startO));
-			out.flush();
+			new CreateImage(input, offset, out, true);
 		}
 		
 		out.close();
